08bbc22e320933625819f86f07f4ba17e8106ce6
Jesus Catalan <jesuscatalan@jesuss-air.lan>
2024-11-26 21:27:28 -0500

    function for readme file done

--- Develop/index.js
+++ Develop/index.js
@@ -53,7 +53,13 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+fs.writeFile(fileName, data, (err) => {
+    if (err) { console.error(`error writing to file`, err);
+    } else {
+        console.log(`${fileName} has been generated successfully!`);
+    }});
+}
 
 // TODO: Create a function to initialize app
 function init() {}
