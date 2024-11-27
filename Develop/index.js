3e26524506592d0c6070ab32a8383dc1e2b923ce
Jesus Catalan <jesuscatalan@jesuss-air.lan>
2024-11-26 20:59:16 -0500

    done with first part

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,54 @@ const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: `input`,
+        name: `title`,
+        message: `what is the title of your project`,
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `provide a description of your project:`,
+    },
+    {
+        type: `input`,
+        name: `installation`,
+        message: `provide installation instructions:`
+    },
+    {
+        type: `input`,
+        name: `usage`,
+        message: `provide usage information`,
+    },
+    {
+        type: `input`,
+        name: `contributing`,
+        message: `provide contribution guidelines:`,
+    },
+    {
+        type: `input`,
+        name: `tests`,
+        message: `provide test instructions:`,
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `choose a license for your project:`,
+        choices: [`MIT`, `Apache 2.0`, `GPL`, `none`],
+    },
+    { 
+        type: `input`,
+        name: `github`,
+        message: `enter your GitHub username:`,
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `enter your email address:`,
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
